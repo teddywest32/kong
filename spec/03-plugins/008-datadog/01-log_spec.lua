ab718b909a6ffee6dcc9bd1e2eacfab6ddb70f77
Shashi Ranjan <shashi@mashape.com>
2016-08-06 10:24:51 -0700

    feat(plugin/datadog) added new metric upstream_latency and tagging support

--- spec/03-plugins/008-datadog/01-log_spec.lua
+++ spec/03-plugins/008-datadog/01-log_spec.lua
@@ -9,13 +9,15 @@ describe("Plugin: datadog (log)", function()
 
     local api1 = assert(helpers.dao.apis:insert {request_host = "datadog1.com", upstream_url = "http://mockbin.com"})
     local api2 = assert(helpers.dao.apis:insert {request_host = "datadog2.com", upstream_url = "http://mockbin.com"})
+    local api3 = assert(helpers.dao.apis:insert {request_host = "datadog3.com", upstream_url = "http://mockbin.com"})
 
     assert(helpers.dao.plugins:insert {
       name = "datadog",
       api_id = api1.id,
       config = {
         host = "127.0.0.1",
-        port = 9999
+        port = 9999,
+        tags = {}
       }
     })
     assert(helpers.dao.plugins:insert {
@@ -24,8 +26,22 @@ describe("Plugin: datadog (log)", function()
       config = {
         host = "127.0.0.1",
         port = 9999,
-        metrics = "request_count,status_count"
+        metrics = "request_count,status_count",
+        tags = {}
       }
+    })
+    assert(helpers.dao.plugins:insert {
+      name = "datadog",
+      api_id = api3.id,
+      config = {
+        host = "127.0.0.1",
+        port = 9999,
+        metrics = "request_count,status_count",
+        tags = {
+          request_count = {"T1:V1"},
+          status_count = {"T2:V2,T3:V3,T4"}
+        }
+      }  
     })
   end)
   teardown(function()
@@ -42,7 +58,7 @@ describe("Plugin: datadog (log)", function()
         server:setoption("reuseaddr", true)
         server:setsockname("127.0.0.1", 9999)
         local gauges = {}
-        for i = 1, 5 do
+        for i = 1, 6 do
           gauges[#gauges+1] = server:receive()
         end
         server:close()
@@ -62,12 +78,13 @@ describe("Plugin: datadog (log)", function()
 
     local ok, gauges = thread:join()
     assert.True(ok)
-    assert.equal(5, #gauges)
+    assert.equal(6, #gauges)
     assert.contains("kong.datadog1_com.request.count:1|c", gauges)
     assert.contains("kong.datadog1_com.latency:%d+|g", gauges, true)
     assert.contains("kong.datadog1_com.request.size:%d+|g", gauges, true)
     assert.contains("kong.datadog1_com.request.status.200:1|c", gauges)
     assert.contains("kong.datadog1_com.response.size:%d+|g", gauges, true)
+    assert.contains("kong.datadog1_com.upstream_latency:%d+|g", gauges, true)
   end)
 
   it("logs only given metrics", function()
@@ -103,4 +120,40 @@ describe("Plugin: datadog (log)", function()
     assert.contains("kong.datadog2_com.request.count:1|c", gauges)
     assert.contains("kong.datadog2_com.request.status.200:1|c", gauges)
   end)
+  
+  it("logs metrics with tags", function()
+    local thread = threads.new({
+      function()
+        local socket = require "socket"
+        local server = assert(socket.udp())
+        server:settimeout(1)
+        server:setoption("reuseaddr", true)
+        server:setsockname("127.0.0.1", 9999)
+        local gauges = {}
+        for i = 1, 2 do
+          gauges[#gauges+1] = server:receive()
+        end
+        server:close()
+        return gauges
+      end
+    })
+    thread:start()
+
+    local res = assert(client:send {
+      method = "GET",
+      path = "/status/200",
+      headers = {
+        ["Host"] = "datadog3.com"
+      }
+    })
+    assert.res_status(200, res)
+
+    local ok, gauges = thread:join()
+    assert.True(ok)
+    assert.equal(2, #gauges)
+    assert.contains("kong.datadog3_com.request.count:1|c|#T1:V1", gauges)
+    assert.contains("kong.datadog3_com.request.status.200:1|c|#T2:V2,T3:V3,T4", gauges)
+  end)
+  
+  
 end)
